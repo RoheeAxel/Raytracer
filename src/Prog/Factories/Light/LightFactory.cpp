0f55e8780bf56a6a1eb26b17380ea470c734b548
lucas <lucas1.dupont@epitech.eu>
2023-05-03 16:24:48 +0200

    feat(LightFactory): Add LightFactory

---
+++ src/Prog/Factories/Light/LightFactory.cpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** final
+** File description:
+** LightFactory.cpp
+*/
+
+#include "LightFactory.hpp"
+
+namespace Raytracer {
+    LightFactory::LightFactory() {
+        this->_dirName = "lights";
+    }
+
+    std::shared_ptr<ILight> LightFactory::getFromBuiltin(const std::string &name, const std::string &options) {
+        if (name == "point")
+            return std::make_shared<PointLight>(options);
+        else if (name == "directional")
+            return std::make_shared<DirLight>(options);
+        else if (name == "ambient")
+            return std::make_shared<AmbientLight>(options);
+        else
+            return nullptr;
+    }
+} // Raytracer

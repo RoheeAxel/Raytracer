ca78d7f88f6fb0701dcc517b1132afafdeefca99
RoheeAxel <axel.rohee@epitech.eu>
2023-05-03 18:28:22 +0200

    Feat (base renderer): add bvh not dynamical, and starting to implement texture

---
+++ src/Prog/Shapes/BVH/BVH.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2023
+** B-OOP-400-REN-4-1-raytracer-axel.rohee
+** File description:
+** BVH
+*/
+
+#ifndef BVH_HPP_
+#define BVH_HPP_
+
+#include "AShape.hpp"
+#include "AABB.hpp"
+
+namespace Raytracer {
+class AABB;
+class BVH : public AShape{
+    public:
+        BVH() = default;
+
+        ~BVH() override = default;
+
+        HitRecord intersection(Ray ray) override;
+        AABB getAABB() override;
+
+    private:
+        std::shared_ptr<IMaterial> _material;
+        std::shared_ptr<AShape> _left;
+        std::shared_ptr<AShape> _right;
+        AABB _box;
+};
+}
+
+#endif /* !BVH_HPP_ */
